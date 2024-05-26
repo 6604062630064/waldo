c43da2619fe0e1ac130e0d3a8d05005e806e0b89
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2024-05-26 15:20:22 +0700

    finish leaderboard route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,15 @@
 import express, { Express, Request, Response } from "express";
 const leaderboardRouter = require("./routes/leaderboard");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const corsOptions = {
+	origin: "http://localhost:3000",
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
+app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(express.urlencoded({ extended: true }));
 app.use("/leaderboard", leaderboardRouter);
--- frontend/src/app/leaderboard/page.tsx
+++ frontend/src/app/leaderboard/page.tsx
@@ -1,3 +1,47 @@
-export default function LeaderBoard() {
-	return "xdddd";
+type scoreObjectType = {
+	id: number;
+	name: string;
+	time_spent: number;
+	timestamp: Date;
+};
+
+const getLeaderboard: () => Promise<null | scoreObjectType[]> = async () => {
+	const response = await fetch(`${process.env.HOST}/leaderboard`, {
+		method: "GET",
+		mode: "cors",
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		return data;
+	} else {
+		return null;
+	}
+};
+
+export default async function LeaderBoard() {
+	const response: null | scoreObjectType[] = await getLeaderboard();
+
+	return (
+		<div className="flex justify-center pt-7">
+			<div className="w-full max-w-screen-lg px-[min(1.5rem,2vw-5px)]">
+				<table className="box-border w-full border-collapse overflow-hidden rounded-2xl">
+					<thead className="h-16 bg-[#6d7ae0] text-white">
+						<tr>
+							<th className="pl-8 text-left font-normal tracking-wide">Name</th>
+							<th className="text-left font-normal tracking-wide">Time</th>
+						</tr>
+					</thead>
+					<tbody>
+						{response?.map((e) => (
+							<tr className="h-12 hover:bg-[#ecedff]">
+								<td className="pl-8">{e.name}</td>
+								<td>{e.time_spent}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
